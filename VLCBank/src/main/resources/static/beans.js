928b05e40bc20f4ebdcbc5e054a87db0fe99e1d4
prageethmahendra <prageethmahendra@gmail.com>
2016-10-28 14:29:15 +0800

    ng-includes added

---
+++ VLCBank/src/main/resources/static/beans.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Prageeth on 28/10/2016.
+ */
+var backUser = {
+    "userName": "Mark",
+    "password": "Galea",
+    "id": 30,
+    "authorized":true
+}
--- VLCBank/src/main/resources/static/controller.js
+++ VLCBank/src/main/resources/static/controller.js
@@ -1,12 +1,13 @@
 /**
  * Created by prageeth.g on 27/10/2016.
  */
-var model = angular.module('myMainAPP', ['ngRoute']);
+var model = angular.module('MainAPP', ['ngRoute']);
+//Main Routing Table
 model.config(function ($routeProvider) {
     $routeProvider.
         when('/', {
-            templateUrl: '/main.html',
-            controller: 'myController'
+            templateUrl: '/loginUser.html',
+            controller: 'loginController'
         })
         .when('/mainPage', {
             resolve: {"check": authCheck},
@@ -19,22 +20,24 @@ model.config(function ($routeProvider) {
             controller: 'createBranchController'
         }).
         otherwise({
-            redirectTo: '/main.html'
+            redirectTo: '/loginUser.html'
         });
-})
+});
 
-model.controller("myController", function ($scope, $location, $log, $http, $rootScope) {
+
+//Login controller authenticates the user
+model.controller("loginController", function ($scope, $location, $log, $http, $rootScope) {
     $log.info($location.path());
 
     var successfullyLogin = function (response) {
         $log.info(response.data);
         $location.path("/mainPage");
 
-    }
+    };
 
     var errorInLogin = function (response) {
         $log.info(response.data);
-    }
+    };
 
     $scope.loginSubmit = function () {
         var username = $scope.userName;
@@ -47,6 +50,7 @@ model.controller("myController", function ($scope, $location, $log, $http, $root
     }
 });
 
+// This handles different tool bar functions
 model.controller("toolBarController", function ($scope, $location, $log) {
     $log.info($location.path());
     $scope.addBranch = function () {
@@ -69,17 +73,22 @@ model.controller("toolBarController", function ($scope, $location, $log) {
     };
 });
 
+// this handles create branch form submit request
 model.controller("createBranchController", function ($scope, $location, $log, $http, $rootScope) {
 
     $log.info($location.path());
     var successfullyLogin = function (response) {
         $log.info(response.data);
-        $location.path('/mainPage');
-    }
+        // var obj = JSON.parse (response.data)
+       // if(obj.authorized) {
+           $location.path('/mainPage');
+       // }
+    };
 
     var errorInLogin = function (response) {
         $log.info(response.data);
-    }
+        $scope.error = "Error in communicating with the server"
+    };
 
     $scope.createBranch = function () {
         var entityContext = {"entityType": "Branch",
@@ -96,14 +105,12 @@ model.controller("createBranchController", function ($scope, $location, $log, $h
     }
 });
 
+// this global function check for authenticated users. If not redirect to login page
 var authCheck = function ($log, $location, $rootScope) {
     $log.info($rootScope.userName);
     if (!$rootScope.userName || !$rootScope.password || $rootScope.userName.length == 0 ||
         $rootScope.password.length == 0) {
         $location.path("/");
     }
-}
-
-//model.value('clientId', { value: 'a12345654321x' });
-
+};
 
